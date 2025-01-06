394e3298beacdfcfe6454ff0e4ee0f005670f9e1
Tushig <ttushig476@gmail.com>
2025-01-06 18:13:18 +0800

    backend-up

--- routers/post-router.js
+++ routers/post-router.js
@@ -1,7 +1,6 @@
 import express from "express";
 import PostModel from "../models/post-model.js";
-import { authMiddleware } from "../middlewares/auth-middlewares.js";
-
+import { authMiddleware } from "../middlewares/auth-middleware.js";
 
 const router = express.Router();
 
@@ -11,10 +10,26 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", authMiddleware, async (req, res) => {
-  const user = req.user;
-  const { description, mediaUrl } = req.body;
-  const newPost = await PostModel.create({ description, mediaUrl, user: user._id });
-  return res.send(newPost);
+  try {
+    const user = req.user;
+    const { description, mediaUrl } = req.body;
+    const newPost = await PostModel.create({ description, mediaUrl, user: user._id });
+    return res.send(newPost);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Server can't handle that request" });
+  }
+});
+
+router.get("/user/:_id", async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const posts = await PostModel.find({ user: _id });
+    return res.send(posts);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Server can't handle that request" });
+  }
 });
 
 export default router;
